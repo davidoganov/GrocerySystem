7c388555cfaa4083dce8881dc8173d7dd33cc9a1
davidoganov <112603130+davidoganov@users.noreply.github.com>
2023-03-30 18:30:04 -0700

    Add files via upload

---
+++ src/radiantgrocery/GroceryPSQL.sql
@@ -0,0 +1,11 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  sonic
+ * Created: Mar 8, 2023
+ */
+
+select * from inventory where sku = 'oreochocol';
+
